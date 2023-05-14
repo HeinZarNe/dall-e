2f2ef9ba1f332965a0b1bd39afbef61c8284b673
HeinZarNe <heinzarne2522005@gmail.com>
2023-05-15 01:49:03 +0630

    error refix

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
 import { surpriseMePrompts } from "../constants";
-import { saveAs } from "file-saver";
+import FileSaver from "file-saver";
 
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -8,19 +8,5 @@ export function getRandomPrompt(prompt) {
 }
 
 export async function downloadImage(_id, photo) {
-  // saveAs(photo, `download-${_id}.jpg`);
-  try {
-    const response = await fetch(photo);
-    const blob = await response.blob();
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = `download-${_id}.jpg`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
-  } catch (error) {
-    console.error("Error downloading image:", error);
-  }
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
